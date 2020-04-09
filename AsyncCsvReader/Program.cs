66bff1cfae11744e61ea587ff22cf18b95fb6905
Sam Gwilliam <sambc49@gmail.com>
2020-04-09 14:47:01 +0100

    Initial check-in of module AsyncCsvReader

---
+++ AsyncCsvReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncCsvReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
