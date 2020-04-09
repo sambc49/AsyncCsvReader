39c513b845ed2e456693449806ab168f867c631b
Sam Gwilliam <sambc49@gmail.com>
2020-04-09 14:49:58 +0100

    Adding model for Employee

---
+++ AsyncCsvReader/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AsyncCsvReader.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public int Salary { get; set; }
+    }
+}
